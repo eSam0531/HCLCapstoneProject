12fe5552d7508409e777323f1107a44382893258
eSam0531 <esama01@jaguar.tamu.edu>
2022-02-28 08:00:35 -0600

    added JPA Repositorys for entities

--- backend/CapstoneEcommerce/src/main/java/com/hcl/model/Product.java
+++ backend/CapstoneEcommerce/src/main/java/com/hcl/model/Product.java
@@ -15,11 +15,15 @@ import javax.persistence.Table;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="product")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 	
 	@Id
