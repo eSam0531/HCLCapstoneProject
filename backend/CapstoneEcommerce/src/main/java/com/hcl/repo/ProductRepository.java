d12c19a153d10e2b7fa548c90d93cd121e8906d1
eSam0531 <esama01@jaguar.tamu.edu>
2022-02-28 15:23:07 -0600

    added crossorigin annotation to ProductRepository for proper API call, initialized front end and started working on product list component

--- backend/CapstoneEcommerce/src/main/java/com/hcl/repo/ProductRepository.java
+++ backend/CapstoneEcommerce/src/main/java/com/hcl/repo/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.hcl.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.hcl.model.Product;
 
+// CrossOrigin annotation allows API to accept call from web browser scripts from this origin
+// scheme/protocol+hostname+port
+// CrossOrigin annotation alone will allow any website to connect to API
+@CrossOrigin("http://localhost:4200") 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
