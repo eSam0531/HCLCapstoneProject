0a3416d97154c1bd211cb71338bfa9511ba3db59
eSam0531 <esama01@jaguar.tamu.edu>
2022-02-28 14:09:42 -0600

    changes to product and product category for readability, creation of MyDataRestConfig to make app read-only temporarily

--- backend/CapstoneEcommerce/src/main/java/com/hcl/model/ProductCategory.java
+++ backend/CapstoneEcommerce/src/main/java/com/hcl/model/ProductCategory.java
@@ -24,8 +24,10 @@ public class ProductCategory {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id")
 	private Long id;
+	
 	@Column(name="category_name")
 	private String categoryName;
+	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy= "category")
 	private Set<Product> products;
 	
