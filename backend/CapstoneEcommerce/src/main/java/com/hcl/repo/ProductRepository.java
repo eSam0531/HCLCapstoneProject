f74862373fdac830feb39b504c8cb9676830072f
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-01 07:56:48 -0600

    added note about crossorigin annotation for personal reference

--- backend/CapstoneEcommerce/src/main/java/com/hcl/repo/ProductRepository.java
+++ backend/CapstoneEcommerce/src/main/java/com/hcl/repo/ProductRepository.java
@@ -7,7 +7,7 @@ import com.hcl.model.Product;
 
 // CrossOrigin annotation allows API to accept call from web browser scripts from this origin
 // scheme/protocol+hostname+port
-// CrossOrigin annotation alone will allow any website to connect to API
+// If CrossOrigin annotation if used alone will allow any website to connect to API
 @CrossOrigin("http://localhost:4200") 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
