5dbe6dde8cae06c365855521eb347bc6ba233cff
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-02 14:45:01 -0600

    added search by category id to filter items with links on navigation bar

--- backend/CapstoneEcommerce/src/main/java/com/hcl/repo/ProductRepository.java
+++ backend/CapstoneEcommerce/src/main/java/com/hcl/repo/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.hcl.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.hcl.model.Product;
 
@@ -11,4 +14,6 @@ import com.hcl.model.Product;
 @CrossOrigin("http://localhost:4200") 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+	
 }
--- frontend/angular-ecommerce/src/app/common/product.ts
+++ frontend/angular-ecommerce/src/app/common/product.ts
@@ -1,13 +1,13 @@
 export class Product {
-    // added ! after property name to allow declaration without initialization
-    sku!: string;
-    name!: string;
-    description!: string;
-    unitPrice!: number;
-    imageUrl!: string;
-    active!: boolean;
-    unitsInStock!: number;
-    dateCreated!: Date;
-    lastUpdate!: Date;
+    
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    dateCreated: Date;
+    lastUpdate: Date;
 }
 
