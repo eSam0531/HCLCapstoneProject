4be10def75f16f5cd465687f413479b1bdcaa864
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-02 16:49:45 -0600

    search bar made functional utilizing the findbyNameContaining method in product repo

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -14,31 +14,51 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId!: number;
+  searchMode: boolean;
 
-  constructor(private productService:ProductService, private route:ActivatedRoute) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(()=>{
+    this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
   }
 
   listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+    }
+  }
+
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    // search for products using the keyword
+    this.productService.searchProducts(theKeyword).subscribe(data=>{
+      this.products = data;
+    });
+  }
+
+  handleListProducts() {
     //check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
-     if(hasCategoryId){
-       // get the "id" param string. convert sting to a number using the "+" symbol
-       this.currentCategoryId=+this.route.snapshot.paramMap.get("id");
-     }
-     else {
-       //no category id avaialbe... default to category id 1
-       this.currentCategoryId=1;
-     }
+    if (hasCategoryId) {
+      // get the "id" param string. convert sting to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get("id");
+    }
+    else {
+      //no category id avaialbe... default to category id 1
+      this.currentCategoryId = 1;
+    }
 
-     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(data=> {
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(data => {
       this.products = data;
-    })
+    });
   }
 
 }
