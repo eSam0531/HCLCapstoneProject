f50728caee9b178dcc238a1ded94ad9a9bfe974b
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-12 18:22:36 -0600

    rm config file for security

--- frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-
-    oidc: {
-        clientId: '0oa44q601yS1B1PIk5d7',
-        issuer: 'https://dev-05568850.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
-        scopes: ['openid', 'profile', 'emial']
-    }
-}
