393dbccb885e2483c57ab14d94ff37cdb448f83d
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-13 21:14:46 -0500

    added members only page routed with oktaauthguard

--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -14,13 +14,14 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { LoginStatusComponent } from './components/login-status/login-status.component';
-import { OKTA_CONFIG, OktaAuthModule, OktaCallbackComponent } from '@okta/okta-angular';
+import { OKTA_CONFIG, OktaAuthModule, OktaCallbackComponent, OktaAuthGuard } from '@okta/okta-angular';
 import myAppConfig from './config/my-app-config';
 import { ProductService } from './services/product.service';
 import { OktaAuth } from '@okta/okta-auth-js';
+import { MemebersPageComponent } from './components/memebers-page/memebers-page.component';
 
 const oktaConfig = Object.assign({
-  onAuthRequired: (injector) => {
+  onAuthRequired: (oktaAuth, injector) => {
     const router = injector.get(Router);
     //Redirect the user to your customer login page
     router.navigate(['/login']);
@@ -30,6 +31,7 @@ const oktaConfig = Object.assign({
 const oktaAuth = new OktaAuth(oktaConfig);
 
 const routes: Routes =[
+  {path: 'members', component:MemebersPageComponent, canActivate: [ OktaAuthGuard ]},
   {path: 'login/callback', component:OktaCallbackComponent},
   {path: 'login', component:LoginComponent},
   {path: 'checkout', component:CheckoutComponent},
@@ -54,7 +56,8 @@ const routes: Routes =[
     CartDetailsComponent,
     CheckoutComponent,
     LoginComponent,
-    LoginStatusComponent
+    LoginStatusComponent,
+    MemebersPageComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
--- frontend/angular-ecommerce/src/app/components/login/login.component.ts
+++ frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { OktaAuthStateService, OKTA_AUTH } from '@okta/okta-angular';
-import { AuthState, OktaAuth } from '@okta/okta-auth-js';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 import * as OktaSignIn from '@okta/okta-signin-widget';
 import myAppConfig from '../../config/my-app-config';
 
@@ -13,8 +13,7 @@ export class LoginComponent implements OnInit {
 
   oktaSignin: any;
 
-  constructor(private oktaAuthService: OktaAuthStateService, 
-              @Inject(OKTA_AUTH) private _oktaAuth: OktaAuth) { 
+  constructor(@Inject(OKTA_AUTH) private _oktaAuth: OktaAuth) { 
     this.oktaSignin = new OktaSignIn({
       logo: 'assets/images/logo.png',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
