c2505d48079365d943e910aaba035f0f674d9c8e
eSam0531 <esama01@jaguar.tamu.edu>
2022-03-22 14:18:09 -0500

    remove config file

--- frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-
-    oidc: {
-        clientId: '0oa469x5u90cMeDHj5d7',
-        issuer: 'https://dev-9601365.okta.com/oauth2/default',
-        redirectUri: 'https://localhost:4200/login/callback',
-        scopes: ['openid', 'profile', 'email']
-    }
-}
