f5a925ea04ce1cc624c154203b00e613cac1d465
eSam0531 <esama01@jaguar.tamu.edu>
2022-04-04 13:04:53 -0500

    updates to add frontend deployment setup and set backend CORS to our new frontend deployment

---
+++ frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa469x5u90cMeDHj5d7',
+        issuer: 'https://dev-9601365.okta.com/oauth2/default',
+        redirectUri: 'https://capstonefrontendtestdeploy.azurewebsites.net/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
--- frontend/angular-ecommerce/src/environments/environment.qa.ts
+++ frontend/angular-ecommerce/src/environments/environment.qa.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://localhost:9898/api"
+  //apiUrl: "https://localhost:8443/api",
+  apiUrl: "https://capstonetestdeploy.azurewebsites.net/api",
+  stripePublishableKey: "pk_test_51KgEObJGMZlRdDhJyPpRMxIVSoEP3hrl2p3LrMmKOYdp6jY8lGUvq3f2KFj4Riti9iB05TaBYKdYu2POuTn0guB700BDbuUHAn"
 };
 
 /*
